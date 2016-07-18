e6f751dc2ba8e3e06f92ed7c300201e12b934309
Philipp Seibt <seibt@muesliplus.de>
2016-07-18 11:55:34 +0200

    change layout by pid

--- src/Resources/contao/classes/AmphtmlHooks.php
+++ src/Resources/contao/classes/AmphtmlHooks.php
@@ -20,25 +20,24 @@ class AmphtmlHooks extends \Controller
     /*
      * if amp is set, load the new fe_page template
      */
-    public function getPageLayout(\PageModel $objPage, \LayoutModel $objLayout, \PageRegular $objPageRegular)
+    public function getPageLayout($objPage, &$objLayout, $objPageRegular)
     {
         if(isset($_GET['amp'])) {
 
-            $objLayout->template = "fe_page_amphtml";
-            $objLayout->templates = "templates/amphtml";
-            //$objLayout->templates = "templates/amphtml";
+            $objLayout = \LayoutModel::findByPid(7);
 
-            //$objLayout->pid->templates = "templates/amphtml";
-            print_r($objLayout);
+            $objPage->layout = 5;
+            print_r($objPage);
+
 
             //inline css
-            echo "http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml_custom.css";
+            /*echo "http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml_custom.css";
             if(file_exists("../files/amphtml/amphtml_custom.css")) {
                 $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml_custom.css");
             } else {
                 $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml.css");
             }
-            $objLayout->head = "<style amp-custom>".$amphtmlCss."</style>";
+            $objLayout->head = "<style amp-custom>".$amphtmlCss."</style>";*/
         }
 
     }
