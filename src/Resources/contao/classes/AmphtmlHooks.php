3fa85f98e1424b234f12cfbbd8d3fbd90bd86adf
Mathias Arzberger <develop@pdir.de>
2016-07-20 13:12:45 +0200

    fix url problem with detail pages

--- src/Resources/contao/classes/AmphtmlHooks.php
+++ src/Resources/contao/classes/AmphtmlHooks.php
@@ -31,29 +31,55 @@ class AmphtmlHooks extends \Controller
     public function ampGetPageLayout($objPage, &$objLayout, $objPageRegular)
     {
         $amphtml = (int) \PageModel::findByPk($objPage->rootId)->amphtml;
-        if ($amphtml && ($page = \PageModel::findByPk($objPage->id)) !== null) {
-            $strUrl = \Controller::generateFrontendUrl($page->row());
-            $objLayout->head .= '<link rel="amphtml" href="' . $strUrl . '?amp" />';
-        }
-
-        if(isset($_GET['amp'])) {
-
-            $ampLayout = (int) \PageModel::findByPk($objPage->rootId)->ampLayout;
-            $ampUseInLayout = \PageModel::findByPk($objPage->rootId)->ampUseInLayout;
-
-            $objLayout = \LayoutModel::findById($ampLayout);
-
-            // load inline css from file or use user custom
-            if(file_exists("../files/amphtml/amphtml_custom.css")) {
-                $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml_custom.css");
-            } else {
-                $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml.css");
+        if($amphtml == 1) {
+            if ($amphtml && ($page = \PageModel::findByPk($objPage->id)) !== null) {
+                $arrSearch = array("?amp", "&amp");
+                $strUrl = str_replace($arrSearch, "", "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s://" : "://")."{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
+                // @ToDo find contao way with generateFrontendUrl
+                // $strUrl = \Controller::generateFrontendUrl($page->row());
+                $objLayout->head .= '<link rel="amphtml" href="' . $strUrl . '?amp" />';
             }
-            $objLayout->head = "<style amp-custom>".$amphtmlCss."</style>";
-            $objLayout->head .= '<link rel="canonical" href="'.$strUrl.'" />';
 
+            if(isset($_GET['amp'])) {
+                $ampLayout = (int) \PageModel::findByPk($objPage->rootId)->ampLayout;
+                $ampUseInLayout = \PageModel::findByPk($objPage->rootId)->ampUseInLayout;
+                $objLayout = \LayoutModel::findById($ampLayout);
+
+                // enable or disable columns in layout
+                $desMod = deserialize($objLayout->modules);
+                for($i = 0; $i <= count($desMod); $i++) {
+                    if( stristr($ampUseInLayout,"head") && $desMod[$i]['col'] == "header" ) {
+                        $desMod[$i]['enable'] = '1';
+                    } else if( !stristr($ampUseInLayout,"head") && $desMod[$i]['col'] == "header" ) {
+                        $desMod[$i]['enable'] = '0';
+                    }
+                    if( stristr($ampUseInLayout,"footer") && $desMod[$i]['col'] == "footer" ) {
+                        $desMod[$i]['enable'] = '1';
+                    } else if( !stristr($ampUseInLayout,"footer") && $desMod[$i]['col'] == "footer" ) {
+                        $desMod[$i]['enable'] = '0';
+                    }
+                    if( stristr($ampUseInLayout,"left") && $desMod[$i]['col'] == "left" ) {
+                        $desMod[$i]['enable'] = '1';
+                    } else if( !stristr($ampUseInLayout,"left") && $desMod[$i]['col'] == "left" ) {
+                        $desMod[$i]['enable'] = '0';
+                    }
+                    if( stristr($ampUseInLayout,"right") && $desMod[$i]['col'] == "right" ) {
+                        $desMod[$i]['enable'] = '1';
+                    } else if( !stristr($ampUseInLayout,"right") && $desMod[$i]['col'] == "right" ) {
+                        $desMod[$i]['enable'] = '0';
+                    }
+                } $objLayout->modules = serialize($desMod);
+
+                // load inline css from file or use user custom
+                if(file_exists("../files/amphtml/amphtml_custom.css")) {
+                    $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml_custom.css");
+                } else {
+                    $amphtmlCss = file_get_contents("http://".$_SERVER['HTTP_HOST']."/files/amphtml/amphtml.css");
+                }
+                $objLayout->head = "<style amp-custom>".$amphtmlCss."</style>";
+                $objLayout->head .= '<link rel="canonical" href="'.$strUrl.'" />';
+            }
         }
-
     }
     /*
      * if amp is set, add amp param to all urls
